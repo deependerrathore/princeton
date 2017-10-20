f9b679868ad136e3f0d50d3f691f106c07b08680
deependerrathore <deepenrathore28@gmail.com>
2017-10-20 11:04:10 +0530

    Distance displayed in correct format

--- Distance.java
+++ Distance.java
@@ -23,6 +23,6 @@ public class Distance{
 
 		double distance = Math.sqrt(Math.pow(x,2) + Math.pow(y,2));
 
-		System.out.println(distance);
+		System.out.println("the distance between ("+ x + "," + y + ") to (0,0) is " + distance);
 	}
 }
