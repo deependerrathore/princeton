c8ba2b903117332bd47bd0fd734cc8f3863dd8af
deependerrathore <deepenrathore28@gmail.com>
2017-10-28 09:27:00 +0530

    Orelly - first project setup

---
+++ First Project/src/orelly/firstproject/Main.java
@@ -0,0 +1,19 @@
+package orelly.firstproject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Main m = new Main();
+		
+		m.run();
+
+	}
+
+	private void run() {
+		
+		System.out.println("Orelly");
+		
+	}
+
+}
