99af0057114a556d01b3dbcb1c90d5f36de5f2f3
deependerrathore <deepenrathore28@gmail.com>
2017-10-28 09:50:16 +0530

    Calling Methods

--- First Project/src/orelly/firstproject/Main.java
+++ First Project/src/orelly/firstproject/Main.java
@@ -6,15 +6,16 @@ public class Main {
 		
 		Main m = new Main();
 		
-		m.run();
+		m.run(); //we can only call static method from static context , for non-static method to call we need a object
 
 	}
 
 	private void run() {
 		
 		Person p = new Person();
-		
-		
+		p.setFirstName("Deepender");
+		p.setLastName("Rathore");
+		System.out.println(p.getFullName());
 		
 	}
 
